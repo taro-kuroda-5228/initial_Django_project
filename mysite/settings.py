a5b70f8dca658d14c19a4fc18ed715b54cd8bcdd
taro-kuroda-5228 <dancing205228@gmail.com>
2022-01-06 21:16:26 +0900

    Update:initial commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,6 +130,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
